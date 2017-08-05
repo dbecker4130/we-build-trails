41b62bb19f4ad7a8e59e90af13ed46b99fbee228
Daniel Becker <dbecker4130@gmail.com>
2017-08-04 21:37:05 -0700

    router config file complete with 3 views

---
+++ app/config/router-config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig];
+
+function routerConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.when('', '/join#signup');
+  $urlRouterProvider.when('/', 'join#signup');
+  $urlRouterProvider.when('/login', '/join#signin');
+
+  let states = [
+    {
+      name: 'home',
+      url: '/home',
+      template: require('../view/home/home.html'),
+      controller: 'HomeController',
+      controllerAs: 'homeCtrl'
+    },
+    {
+      name: 'landing',
+      url: '/join',
+      template: require('../view/landing/landing.html'),
+      controller: 'LandingController',
+      controllerAs: 'landingCtrl'
+    },
+    {
+      name: 'profile',
+      url: '/profile',
+      template: require('../view/profile/profile.html'),
+      controller: 'ProfileController',
+      controllerAs: 'profileCtrl'
+    }
+  ];
+
+  states.forEach(state => {
+    $stateProvider.state(state);
+  });
+}
