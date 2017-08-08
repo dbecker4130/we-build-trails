4fa2123d31ea0b75ff68501b190b914d03700f5b
Daniel Becker <dbecker4130@gmail.com>
2017-08-07 17:44:56 -0700

    update and delete methods added to post service

--- app/service/post-service.js
+++ app/service/post-service.js
@@ -38,5 +38,74 @@ function postService($q, $log, $http, authService) {
     });
   };
 
-  
+  service.updatePost = (postID, postData) => {
+    $log.debug('postService.updatePost()');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/post/${postID}`;
+      let config = {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      };
+      return $http.put(url, postData, config);
+    })
+    .then( res => {
+      for (var i = 0; i < service.allPosts.length; i++) {
+        let current = service.allPosts[i];
+        if (current._id === postID) {
+          service.allPosts[i] = res.data;
+          break;
+        }
+      }
+      return res.data;
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  service.deletePost = function(postID) {
+    $log.debug('postService.deletePost()');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/post/${postID}`;
+      let config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+      return $http.delete(url, config);
+    })
+    .then( res => {
+      for (var i = 0; i < service.allPosts.length; i++) {
+        let current = service.allPosts[i];
+        if (current._id === postID) {
+          service.allPosts.splice(i, 1);
+          break;
+        }
+      }
+    })
+    .then( token => {
+      let url = `${__API_URL__}/api/post/${postData._id}/image/${imageData._id}`;
+      let config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      };
+      return $http.delete(url, config);
+    })
+
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+
 }
