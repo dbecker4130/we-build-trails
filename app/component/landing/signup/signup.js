8f70d7a59fb19ccce70941583835ef8e0fedba8a
Daniel Becker <dbecker4130@gmail.com>
2017-08-09 15:55:55 -0700

    sign up and sign in JS files complete

---
+++ app/component/landing/signup/signup.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  template: require('./signup.html'),
+  controller: ['$log', '$location', 'authService', SignupController],
+  controllerAs: 'signupCtrl'
+};
+
+function SignupController($log, $location, authService){
+  $log.debug('SignupController');
+
+  authService.getToken()
+  .then(() => {
+    $location.url('/home');
+  }).catch(err => {
+    $log.error(err.message);
+  });
+
+  this.signup = function(user){
+    $log.log('SignupController.signup()');
+
+    authService.signUp(user)
+    .then(() => {
+      $location.url('/home');
+    }).catch(() => {
+      alert('- Username: 4 characters ' + '\n' + '- Email: 6 characters' + '\n' + '- Password: 6 charaters');
+    });
+  };
+}
