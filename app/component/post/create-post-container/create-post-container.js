49b4ea5a3adfb526286a859a7f2090ac951d2643
Daniel Becker <dbecker4130@gmail.com>
2017-08-21 10:53:29 -0700

    profile view layout changed, edit button temporarily disabled

--- app/component/post/create-post-container/create-post-container.js
+++ app/component/post/create-post-container/create-post-container.js
@@ -4,26 +4,16 @@ require('./_create-post-container.scss');
 
 module.exports = {
   template: require('./create-post-container.html'),
-  controller: ['$log', 'authService', CreatePostContController],
+  controller: ['$log', CreatePostContController],
   controllerAs: 'createPostContCtrl',
   bindings: {
     post: '<',
   }
 };
 
-function CreatePostContController($log, authService) {
+function CreatePostContController($log) {
   $log.debug('CreatePostContController');
 
   this.showCreatePost = false;
-  this.showUploadImage = false;
 
-  this.user = null;
-  this.fetchUsername = function() {
-    authService.fetchUserData()
-    .then(user => {
-      $log.log('got user', user);
-      this.user = user;
-    });
-  };
-  this.fetchUsername();
 }
