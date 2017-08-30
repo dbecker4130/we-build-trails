dd37b70ea9c0687c98a92ce737222a5bc97e1bbc
Daniel Becker <dbecker4130@gmail.com>
2017-08-30 14:28:36 -0700

    upload profile module complete

---
+++ app/component/user/upload-profile/upload-profile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  template: require('./upload-profile.html'),
+  controller: ['$log', 'profileService', UploadProfileImageController],
+  controllerAs: 'uploadProfileImageCtrl',
+  bindings: {
+    user: '<'
+  }
+};
+
+function UploadProfileImageController($log, profileService) {
+  $log.debug('UploadProfileImageController');
+
+  this.image = {};
+
+  this.uploadProfileImage = function(file) {
+    profileService.uploadProfileImage(file)
+    .then(() => {
+      this.image.file = null;
+    })
+    .catch(err => {
+      $log.error(err.message);
+    });
+  };
+}
